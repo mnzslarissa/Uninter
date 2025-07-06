935a66aac9278fea90fcd01a7a9734358ef82b28
mnzslarissa <134641050+mnzslarissa@users.noreply.github.com>
2025-07-06 19:02:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+  const mensagem = document.getElementById("mensagem");
+  mensagem.textContent = "Bem-vindo à minha página! Nunca desista dos seus sonhos!";
+  mensagem.style.display = "block";
+}
